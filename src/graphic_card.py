7d8627dd160d6b13f7c5d2d9c36d7dd73be2ac89
Tamara Kadyear Saber <tamarakadyear@gmail.com>
2025-06-01 12:42:34 +0200

    refactor: refactor graphic card class to enable buy differents amounts of cards and add type config

--- src/graphic_card.py
+++ src/graphic_card.py
@@ -1,25 +1,34 @@
+from typing import TypedDict
+
+class GraphicCardConfig(TypedDict):
+    price: float
+    units: int
+    percentage_fluctuation: float
+
 class GraphicCard:
     def __init__(
         self,
         price: float,
         units: int,
-        price_fluctuation: float,
         percentage_fluctuation: float = 0.5,
     ):
         self.price = price
         self.units = units
-        self.price_fluctuation = price_fluctuation
         self.percentage_fluctuation = percentage_fluctuation
+    
+    @classmethod
+    def from_config(cls, config: GraphicCardConfig) -> "GraphicCard":
+        return cls(**config)
 
-    def calc_price_fluctuation(self):
-        return (self.price * self.percentage_fluctuation) / 100
+    def calc_price_fluctuation(self, amount: int):
+        return (self.price * self.percentage_fluctuation * amount) / 100
 
-    def buy(self):
-        self.units -= 1
-        self.price += self.calc_price_fluctuation()
+    def buy(self, amount: int = 1):
+        self.units -= amount
+        self.price += self.calc_price_fluctuation(amount)
         return self
 
-    def sell(self):
-        self.units += 1
+    def sell(self, amount: int = 1):
+        self.units += amount
         self.price -= self.calc_price_fluctuation()
         return self
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from graphic_card import GraphicCard
+from graphic_card import GraphicCard,GraphicCardConfig
 from utils.utils import create_agents
 from market import Market
 
@@ -13,14 +13,33 @@ initial_units_graphic_card = 100000
 days = 10
 
 price_fluctuation_graphic_card = 0.5
-card = GraphicCard(
-    initial_price_graphic_card,
-    initial_units_graphic_card,
-    price_fluctuation_graphic_card,
-)
-market = Market(
-    card,
-    days,
-    create_agents(number_agents, number_agents_trend, number_agents_antitrend),
-)
-market.startMarket()
+# card = GraphicCard(
+#     initial_price_graphic_card,
+#     initial_units_graphic_card,
+#     price_fluctuation_graphic_card,
+# )
+# market = Market(
+#     card,
+#     days,
+#     create_agents(number_agents, number_agents_trend, number_agents_antitrend),
+# )
+# market.startMarket()
+
+config = {
+    "graphic_card": {"units": 100000, "price": 200, "percentage_fluctuation": 0.5},
+    "agents": {
+        "initial_balance": 1000,
+        "number_random_agents": 51, 
+        "number_antitrend_agents": 24, 
+        "number_trend_agents": 24, 
+        "number_custom_agents": 1, 
+    }
+}
+
+
+class Setup:
+    def __init__(self, graphic_card: GraphicCardConfig):
+        self.graphic_card = GraphicCard.from_config(graphic_card)
+
+
+setup = Setup(**config)
